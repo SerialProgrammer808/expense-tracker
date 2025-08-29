715348714582d5994a2d86c3e979129b4aad70fe
Noah Asano <pili.asano@gmail.com>
2025-08-28 16:33:41 -1000

    Clean up code

--- src/main/java/com/noahasano/expense/controller/ExpenseController.java
+++ src/main/java/com/noahasano/expense/controller/ExpenseController.java
@@ -16,7 +16,6 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
